d5c8972390139aec93c5c2f361c80352c3fc092c
Krys Tyler <krys.tyler@snowflake.com>
2024-11-12 07:23:43 -0800

    Update streamlit_app.py
    left off a close parens

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ if ingredients_list:
 # New section to display smoothiefrrot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response).json()
+st.text(smoothiefroot_response).json())
 
